4f726ebcef7ad3f9472fadca89e8e7d7da0a9889
uid-11222 <uid.11222@gmail.com>
2020-03-21 21:44:16 +0300

    Add grid and labels on graph

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import rawData from './data.js';
 
-const barColor = 'rgba(250, 0, 0, 0.5)';
+const barColor = 'rgba(250, 0, 0, 0.3)';
 const bgColor = 'rgba(0, 0, 100, 0.1)';
-const rowWidth = 20;
-const rowWidthPadding = 10;
-const totalHeight = 600;
+const gridColor = '#999';
+const labelBgColor = 'rgba(0, 300, 300, 0.3)';
+const labelFontSize = 14;
+const labelMargin = 50;
+const labelDistance = 4;
+const heightLabelsStep = 10000;
+const rowWidth = 35;
+const rowWidthPadding = 27;
+const msInDay = 24 * 3600 * 1000;
+const textColor = '#222';
+const totalHeight = 900;
 const year = new Date().getFullYear();
 
 const data = rawData
@@ -23,23 +31,33 @@ const graph = document.getElementById('graph');
 const ctx = graph.getContext('2d');
 
 const startDate = data[0].date;
-const getDaysFromStart = date => (date - startDate) / (24 * 3600 * 1000);
+const getDateString = date => {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(1 + date.getMonth()).padStart(2, '0');
+
+    return `${day}.${month}`;
+};
+const getDaysFromStart = date => (date - startDate) / msInDay;
 const rows = 1 + getDaysFromStart(data[data.length - 1].date);
 const totalWidth = rows * rowWidth;
+const rowHalf = Math.round((rowWidth - rowWidthPadding) / 2);
 
 const max = Math.max(...data.map(item => item.value));
 const heightScale = (totalHeight - 30) / max;
 
-graph.width = totalWidth;
-graph.height = totalHeight;
+graph.width = totalWidth + labelMargin;
+graph.height = totalHeight + labelMargin;
 
 ctx.fillStyle = bgColor;
 ctx.fillRect(0, 0, totalWidth, totalHeight);
+ctx.fillStyle = labelBgColor;
+ctx.fillRect(0, totalHeight, totalWidth + labelMargin, labelMargin);
+ctx.fillRect(totalWidth, 0, labelMargin, totalHeight);
 
 ctx.fillStyle = barColor;
-ctx.lineWidth = Math.round(rowWidth / 10);
+ctx.lineWidth = 2;
 ctx.beginPath();
-// ctx.moveTo(0, totalHeight);
+ctx.font = `${labelFontSize}px mono`;
 
 data.forEach(({ date, value }) => {
     const days = getDaysFromStart(date);
@@ -47,8 +65,34 @@ data.forEach(({ date, value }) => {
     const left = days * rowWidth;
     const top = totalHeight - height;
 
-    ctx.lineTo(left, top);
+    ctx.lineTo(left + rowHalf, top);
     ctx.fillRect(left, top, rowWidth - rowWidthPadding, height);
 });
+
+ctx.stroke();
+
+ctx.beginPath();
+ctx.fillStyle = textColor;
+ctx.strokeStyle = gridColor;
+ctx.lineWidth = 1;
+
+for (let i = 0; i < rows; i += 1) {
+    if (i % labelDistance === 0 || i === rows - 1) {
+        const left = i * rowWidth;
+        const date = new Date(data[0].date.valueOf() + i * msInDay);
+        ctx.fillText(getDateString(date), left, totalHeight + labelFontSize);
+        ctx.moveTo(left, 0);
+        ctx.lineTo(left, totalHeight);
+    }
+}
+
+const heightLabelsCount = 1 + Math.floor(max / heightLabelsStep);
+
+for (let i = 0; i < heightLabelsCount; i += 1) {
+    const height = Math.round(i * heightScale * heightLabelsStep);
+    ctx.fillText(heightLabelsStep * i, totalWidth, totalHeight - height);
+    ctx.moveTo(0, totalHeight - height);
+    ctx.lineTo(totalWidth, totalHeight - height);
+}
 
 ctx.stroke();

bb01683c5482528f5c2c7ea892b894b6b4c7a05d
uid11 <uid.11222@gmail.com>
2020-04-16 11:52:42 +0300

    Add Saint Petersburg

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,6 @@
 import * as data from './data.js';
 import draw from './draw.js';
 
-Object.entries(data).forEach(([name, dataText]) => draw({ dataText, name }));
+Object.entries(data).forEach(([name, dataText]) =>
+    draw({ dataText, name: name.replace('_', ' ') }),
+);

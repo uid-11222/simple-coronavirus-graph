6677a76c6b38392a7f3df85cb41a108847f5a73e
uid11 <uid.11222@gmail.com>
2020-04-16 16:40:02 +0300

    Added Moscow Oblast

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,5 +2,5 @@ import * as data from './data.js';
 import draw from './draw.js';
 
 Object.entries(data).forEach(([name, dataText]) =>
-    draw({ dataText, name: name.replace('_', ' ') }),
+    draw({ dataText, name: name.replace(/_/g, ' ') }),
 );
